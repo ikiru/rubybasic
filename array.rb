f568b894f808e6d77077cc828c5ae5dbed87a002
Jeff Winkler <jeff.a.winkler@gmail.com>
2017-07-31 16:37:02 -0500

    first commit

---
+++ array.rb
@@ -0,0 +1,22 @@
+arr = [1, 2, 3, 4, 5, 6]
+a = arr.at(0)
+puts a
+
+b = arr.fetch(0)
+puts b
+
+c = arr.delete(1)
+puts arr
+
+d = arr.reverse
+puts arrclear
+
+e = arr.length
+puts e
+
+f = arr.sort do |_b, _a|
+  puts f
+
+  g = arr.slice(2, 2)
+  puts g
+end
---
+++ guess_a_number.rb
@@ -0,0 +1,9 @@
+def guess_number guess
+    number = 25
+if guess  < number
+  puts 'Too low'
+elseif guess > number
+  puts "Too High"
+elsif guess = number
+  puts you guessed the  number
+end 
