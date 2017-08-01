60a2be40832870b466455244dca48348e9eec11d
Jeff Winkler <jeff.a.winkler@gmail.com>
2017-08-01 10:19:58 -0500

    range

--- range.rb
+++ range.rb
@@ -1,11 +1,8 @@
 x = (1..5)
 
-puts "Class Name: #{{x.class}}"
+puts "Class Name: #{x.class}"
 
-puts "It does include 3!" If x.include? 3
-puts "the last number of this range is  " + x.last.to_s
-puts "the maximum number of this range is  " + x.max.to_s
-puts "the minimum number of this range is  " + x.min.to_s
-
-
-end
+puts 'It does include 3!' if  x.include? 3
+puts 'the last number of this range is  ' + x.last.to_s
+puts 'the maximum number of this range is  ' + x.max.to_s
+puts 'the minimum number of this range is  ' + x.min.to_s
